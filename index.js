56a841d905d9e20fe8ecc596908c4eae2a084de5
Andrew Crites <ajcrites@gmail.com>
2016-02-25 22:38:35 -0500

    Streaming HTML to improve speed

--- index.js
+++ index.js
@@ -1,66 +1,103 @@
-const Promise = require("bluebird");
-const co = require("co");
-const axios = require("axios");
+'use strict';
+
 const querystring = require("querystring");
-const jsdom = require("jsdom");
+const request = require("request");
+const htmlparser = require("htmlparser2");
 const key = "52633f4973cf845e55b18c8e22ab08d5";
+const searchHost = "http://www.gainesvillemls.com";
 
-co(function* () {
-    const searchHost = "http://www.gainesvillemls.com";
+let inListing = false;
+let mlsNext = false;
+let foundMls = false;
 
-    let responseBody = yield axios({
-        url: `${searchHost}/gan/idx/search.php`,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        data: querystring.stringify(Object.assign({key}, require("./request.json"))),
-    });
-
-    let dfd = Promise.defer();
-    jsdom.env({
-        html: responseBody.data,
-        done: (err, window) => dfd.resolve(window)
-    });
-    let window = yield dfd.promise;
-
-    Array.prototype.forEach.call(window.document.querySelectorAll("table.listings"), elem => co(function* () {
-        if (/gainesville, fl/i.test(elem.querySelector("tr:nth-of-type(3)").textContent)) {
-            const mls = elem.querySelector("span.mls").textContent;
-            let responseBody = yield axios({
-                url: `${searchHost}/gan/idx/detail.php`,
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-                data: querystring.stringify({
-                    key,
-                    mls,
-                    gallery: "false",
-                    custom: "",
-                }),
-            });
-
-            let dfd = Promise.defer();
-            jsdom.env({
-                html: responseBody.data,
-                done: (err, window) => dfd.resolve(window)
-            });
-            let detailsWindow = yield dfd.promise;
-            let hasParking = true;
-            let hasBlock = false;
-
-            Array.prototype.forEach.call(detailsWindow.document.querySelectorAll("table.wide label.bold"), elem => {
-                if ("Parking:" === elem.textContent && /no garage/i.test(elem.parentElement.querySelector("span").textContent)) {
-                    hasParking = false;
+const parser = new htmlparser.Parser({
+    onopentag(name, attribs) {
+        if ("table" === name && "listings" === attribs.class) {
+            inListing = true;
+        }
+        if (inListing && "span" === name && "mls" === attribs.class) {
+            inListing = false;
+            mlsNext = true;
+        }
+    },
+    ontext(text) {
+        if (mlsNext) {
+            mlsNext = false;
+            foundMls = text;
+        }
+        if (foundMls) {
+            if (/, fl/i.test(text)) {
+                // Matches a Gainesville address, so we are intersted
+                // in this mls
+                if (/gainesville, fl/i.test(text)) {
+                    checkMlsDetails(foundMls);
                 }
-                if ("Construction-exterior:" === elem.textContent && /block/i.test(elem.parentElement.querySelector("span").textContent)) {
-                    hasBlock = true;
+                else {
+                    foundMls = false;
                 }
-            });
-            if (hasParking && hasBlock) {
-                console.log(`${searchHost}/gan/idx/index.php?key=${key}&mls=${mls}`);
             }
         }
-    }));
-}).catch(err => console.error(err));
+    },
+});
+
+request.post(`${searchHost}/gan/idx/search.php`).form(
+    querystring.stringify(Object.assign({key}, require("./request.json")))
+).on("data", data => parser.write(data)).on("end", () => parser.end());
+
+function checkMlsDetails(mls) {
+    let foundConstructionLabel = false;
+    let foundConstructionSpan = false;
+    let hasBlockConstruction = false;
+
+    let foundParkingLabel = false;
+    let foundParkingSpan = false;
+    let hasGarageParking = true;
+
+    const parser = new htmlparser.Parser({
+        onopentag(name) {
+            // span that follows the construction label
+            if (foundConstructionLabel && "span" === name) {
+                foundConstructionLabel = false;
+                foundConstructionSpan = true;
+            }
+
+            // span that follows the parking label
+            if (foundParkingLabel && "span" === name) {
+                foundParkingLabel = false;
+                foundParkingSpan = true;
+            }
+        },
+
+        ontext(text) {
+            if ("Construction-exterior:" === text) {
+                foundConstructionLabel = true;
+            }
+            if (foundConstructionSpan) {
+                foundConstructionSpan = false;
+                if (/block/i.test(text)) {
+                    hasBlockConstruction = true;
+                }
+            }
+
+            if ("Parking:" === text) {
+                foundParkingLabel = true;
+            }
+            if (foundParkingSpan) {
+                foundParkingSpan = false;
+                if (/no garage/i.test(text)) {
+                    hasGarageParking = false;
+                }
+            }
+        },
+
+        onend() {
+            if (hasBlockConstruction && hasGarageParking) {
+                console.log(`${searchHost}/gan/idx/index.php?%s`, querystring.stringify({key, mls}));
+            }
+        }
+    });
+
+    request.post(`${searchHost}/gan/idx/detail.php`).form(
+        querystring.stringify({key, mls, gallery: "false", custom: ""})
+    ).on("data", data => parser.write(data)).on("end", () => parser.end());
+}
